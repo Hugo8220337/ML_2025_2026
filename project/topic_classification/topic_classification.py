2480750f7c9312ace6af7506d28bd7eb811b9488
Diogo Pereira <diogo.2002.miguel@gmail.com>
2025-12-05 21:51:05 +0000

    topic_classification

--- project/topic_classification/topic_classification.py
+++ project/topic_classification/topic_classification.py
@@ -1,15 +1,15 @@
 import pandas as pd
 import numpy as np
 from common.tools import read_csv
+from common.supervised_models import train_neural_network
 
 
 
 def topic_classification():
     file_path = 'datasets/AGNEWS/train.csv'
     df = read_csv(file_path)
-    
 
-    print(df.to_json(indent=4))
+
     
 
 
