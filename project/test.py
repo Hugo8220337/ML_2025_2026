5ffcd847a5bea078c8d8940a4271b1dbb2dd1ec1
Diogo Pereira <diogo.2002.miguel@gmail.com>
2026-01-15 16:16:44 +0000

    inference

--- project/test.py
+++ project/test.py
@@ -4,11 +4,23 @@ os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 from frontend.modules.news import get_article
-from inference import predict
+from frontend.modules.inference import predict
 
-n = get_article('https://www.aljazeera.com/news/2026/1/15/ice-officer-shoots-venezuelan-immigrant-in-minneapolis-what-we-know')
+new_list = [
+    'https://theonion.com/powerful-bidet-blasts-hole-clean-through-man/',
+    'https://theonion.com/conservatives-say-renee-good-was-brainwashed-by-bible-into-loving-thy-neighbor/',
+    'https://theonion.com/vivid-sex-dream-about-steely-mcbeam-again/',
+    'https://theonion.com/melania-trump-casts-longtime-aide-into-well-of-gloom/',
+    'https://theonion.com/rfk-jr-coughs-up-pair-of-jeans/'
 
-result = predict(n['title'], n['text'])
-print
+]
+
+for n in new_list:
+    n = get_article(n)
+
+    result = predict(n['title'], n['text'])
+    with open('output.txt', 'w') as f:
+        print(result['final_prediction'].iloc[0], file=f)
+        print(result['confidence'].iloc[0], file=f)
 
 
